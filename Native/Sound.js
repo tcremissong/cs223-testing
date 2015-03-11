2989918e8b3319d80d98360bd192255e94df7d3d
Thomas Remissong <thomas.remissong@gmail.com>
2015-03-11 01:58:54 -0500

    Fixed some stuff

--- Native/Sound.js
+++ Native/Sound.js
@@ -6,38 +6,58 @@ Elm.Native.Sound.make = function(elm) {
 
 	var Utils = Elm.Native.Utils.make(elm);
 	var Signal = Elm.Native.Signal.make(elm);
-	var Maybe = Elm.Native.Maybe.make(elm);
 
 	var Init = {ctor : "Init"};
 	var TimeUpdate = {ctor : "TimeUpdate"};
 	var Ended = {ctor : "Ended"};
-	
-	function AVProperties(currentTime, duration, ended, volume, src) {
+
+	function AVProperties(currentTime, duration, ended, volume, src, filename) {
 		return { _ : {}, currentTime : currentTime, duration : duration, 
-						 ended : ended, volume : volume, src : src };
+						 ended : ended, volume : volume, src : src, filename : filename};
 	}
 
 	function avsignal(eventHandler, avSrc, avHandler, events) {
 		var audio = new Audio(avSrc);
 		var timer = new Object();
 
-		var signal = Signal.constant(Utils.Tuple2(Init, AVProperties(0,0,0,1.0,0)));
-		var process = eventHandler({"audio" : audio, "timer" : timer});
+		var index = 0;
+		var playlist = new Object();
+		var tracklist = [audio.src];
+
+		audio.volume = 1.0;
+
+		var signal = Signal.constant(Utils.Tuple2(Init, AVProperties(0,0,0,0,"","")));
+		var process = eventHandler({"audio": audio, "timer": timer, "playlist": playlist});
 
 		var time;
 
-		function exposeProperty(e) {
-			var p = AVProperties(audio.currentTime, audio.duration, audio.ended, audio.volume, audio.src);
-			elm.notify(process.id, Utils.Tuple2(e, p));
+		var filename = audio.src.split('/').pop();
+
+		function exposeProperties(e) {
+			// if we're at the end of the file, jump to the next track
+			if (audio.currentTime >= audio.duration) {
+				if (index + 1 < tracklist.length) {
+					index = index + 1;
+					audio.src = tracklist[index];
+					time = 0;
+					timer = new Object();
+					filename = audio.src.split('/').pop();
+				}
+			}
+			var p = AVProperties(audio.currentTime, audio.duration, audio.ended, audio.volume, audio.src, filename);
+			elm.notify(signal.id, Utils.Tuple2(e, p));
 			var event = avHandler(p);
 			if (event.ctor == "Just")
 				process(event._x);
 		}
 
-        timer.start = function() { time = setInterval(function(){ exposeProperty(TimeUpdate); }, 100); };
-        timer.stop = function() { clearInterval(timer); };
+        timer.start = function() { time = setInterval(function(){ exposeProperties(TimeUpdate); }, 100); };
+        timer.stop = function() { clearInterval(time); };
 
-        audio.addEventListener('ended', function() { exposeProperty(Ended); });
+        playlist.add = function(str) { tracklist.push(str); };
+        
+
+        audio.addEventListener('ended', function() { exposeProperties(Ended); });
 
         Signal.map(process)(events);
         return signal;
@@ -54,18 +74,41 @@ Elm.Native.Sound.make = function(elm) {
 	}
 
 	function seek(o, t) {
-		o.audio.currentTime = t;
+		o.audio.currentTime = o.audio.currentTime + t;
 	}
 
 	function setVol(o, v) {
-		o.audio.volume = v;
+		o.audio.volume = o.audio.volume + v;
 	}
 
-	function setSrc(o, path) {
+	function volUp(o) {
+		if (o.audio.volume < 1.0)
+			o.audio.volume = o.audio.volume + 0.1;
+	}
+
+	function volDown(o) {
+		if (o.audio.volume > 0.0)
+			o.audio.volume = o.audio.volume - 0.1;
+	}
+
+	// deprecated
+	/*function setSrc(o, path) {
 		o.timer.stop();
 		o.time = 0;
 		o.audio = new Audio(path);
 		play(o);
+	}*/
+
+	function addToPlaylist(o, path) {
+		o.playlist.add(path);
+	}
+
+	function nextTrack(o) {
+		o.audio.currentTime = o.audio.duration;
+	}
+
+	function lastTrack(o) {
+		//o.playlist.back();
 	}
 
 	var vals = {
@@ -74,7 +117,10 @@ Elm.Native.Sound.make = function(elm) {
 		pause : pause,
 		seek : F2(seek),
 		volume : F2(setVol),
-		source : F2(setSrc)
+		volUp : volUp,
+		volDown : volDown,
+		addTrack : F2(addToPlaylist),
+		nextTrack : nextTrack,
 	};
 
 	return elm.Native.Sound.values = vals;
--- Sound.elm
+++ Sound.elm
@@ -1,7 +1,46 @@
 module Sound where
 
+{-| The Sound module provides a bridge to HTML5 audio and some associated properties (it's a work in progress) |-}
+
 import Native.Sound
 import Signal
+import Time (Time)
 
-sound : Int -> Int
-sound x = x + 1
+type alias Volume = Float
+type alias Source = String
+
+{-| A control manages manipulation of the HTML5 audio stream |-}
+type Control = Play | Pause | Seek Time | VolumeUp | VolumeDown | AddTrack Source | Skip | Back | NoChange
+
+{-| Properties allow the elm user to directly view HTML5 audio properties |-}
+type alias Properties = { currentTime : Time, duration : Time, ended : Bool, volume : Volume, src : Source, filename : String}
+
+{-| Events are the conditions upon which Properties are exposed |-}
+type Event = Init | TimeUpdate | Ended
+
+{-| The SoundConstructor alias allows us an easy way to construct Elm HTML5 streams |-}
+type alias SoundConstructor = { source : Source, 
+                                pHandler : (Properties -> Maybe Control), 
+                                controls : Signal Control
+                              }
+
+{-| The eventHandler manages the bridging of controls to the Native backend |-}
+eventHandler sound control = case control of
+    Play -> Native.Sound.play sound
+    Pause -> Native.Sound.pause sound
+    Seek t -> Native.Sound.seek sound t
+    VolumeUp -> Native.Sound.volUp sound
+    VolumeDown -> Native.Sound.volDown sound
+    AddTrack src -> Native.Sound.addTrack sound src
+    Skip -> Native.Sound.nextTrack sound
+    Back -> ()--Native.Sound.lastTrack sound
+    NoChange -> ()
+
+{-| Creates a sound stream that exposes its properties upon each of the Event types above |-}
+sound : SoundConstructor -> Signal (Event, Properties)
+sound s = 
+    Native.Sound.sound 
+        eventHandler
+        s.source
+        s.pHandler
+        s.controls
